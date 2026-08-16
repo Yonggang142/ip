177fdee1804d9ddbc602f5f9a04c7d0230cbc8e4
Yonggang142 <yonggang2004@gmail.com>
2026-08-16 20:24:38 +0800

    Changed the name of chatbot from Duke to Aegis

---
+++ src/main/java/Aegis.java
@@ -0,0 +1,11 @@
+public class Aegis {
+    public static void main(String[] args) {
+        String banner = "    _              _     \n"
+                + "   / \\   ___  __ _(_)___ \n"
+                + "  / _ \\ / _ \\/ _` | / __|\n"
+                + " / ___ \\  __/ (_| | \\__ \\\n"
+                + "/_/   \\_\\___|\\__, |_|___/\n"
+                + "              |___/      \n";
+        System.out.println(banner);
+    }
+}
