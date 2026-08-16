20c2554b82975c735bcdf8de457d4038f6db9d60
Yonggang142 <yonggang2004@gmail.com>
2026-08-16 20:42:15 +0800

    Added support for echo

--- src/main/java/Aegis.java
+++ src/main/java/Aegis.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Aegis {
     public static void main(String[] args) {
         String banner = "    _              _     \n"
@@ -7,10 +8,27 @@ public class Aegis {
                 + "/_/   \\_\\___|\\__, |_|___/\n"
                 + "              |___/      \n";
 
-        String message = "Hi! This is Aegis!\n"
-                + "What can I do for you today?\n"
-                + "Bye. See you soon!";
+        String startMessage = "Hi! This is Aegis!\n"
+                + "What can I do for you today?\n";
+        String endMessage = "Bye. See you soon!";
 
-        System.out.println(banner + "\n" + message);
+        System.out.println(banner + "\n" + startMessage);
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(endMessage);
+
+            } else {
+                System.out.println(command);
+                break;
+            }
+        }
+
     }
 }
