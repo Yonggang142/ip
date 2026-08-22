c2cac68d26d87af662cb2ccae3dfcce2e0879d52
Yonggang142 <e1528189@u.nus.edu>
2026-08-22 20:00:01 +0800

    Added date and time formatting to tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that takes place from a start time to an end time.
  */
 public class Event extends Task {
 
-    protected String start;
+    protected LocalDate start;
 
-    protected String end;
+    protected LocalDate end;
 
     /**
      * Returns void
@@ -16,7 +19,7 @@ public class Event extends Task {
      * @param end End time of the event.
      * @param isDone Whether the task has been marked as done.
      */
-    public Event(String description, String start, String end, boolean isDone) {
+    public Event(String description, LocalDate start, LocalDate end, boolean isDone) {
         super(description, isDone);
         this.start = start;
         this.end = end;
@@ -31,7 +34,8 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[" + getTypeIcon() + "][" + getStatusIcon() + "] " + description + " (from: " + start + " to: " + end + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return "[" + getTypeIcon() + "][" + getStatusIcon() + "] " + description + " (from: " + start.format(formatter) + " to: " + end.format(formatter) + ")";
     }
 
 
